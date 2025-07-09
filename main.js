5652d482f1916073cea23fb5e1573a608d02ab8c
MountainChickadee <osmo13@gmail.com>
2025-07-09 15:38:23 -0600

    createGrid now sizes boxes

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 // grid setup: loop which creates number of squares set by user, default 16 
-function createGrid() {
-    let numSquares = 16 ** 2; // add user input latter
-    for (let index = 0; index < numSquares; index++) {
+function createGrid(userInput) {
+    let gridSize = userInput ** 2;
+    let boxSize = (960 / userInput);
+    for (let index = 0; index < gridSize; index++) {
         let newSquare = document.createElement('div');
         newSquare.setAttribute('class', 'box');
         container.appendChild(newSquare);
+        newSquare.style.width = `${boxSize}px`;
+        newSquare.style.height = `${boxSize}px`;
     }
 } 
 
 let container = document.querySelector('#container');
-createGrid();
+let userInput = 16 // attch to button 
+
+createGrid(userInput);
