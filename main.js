eecb9e426eed6f4895d12333bd0a4fcfa89463e0
MountainChickadee <osmo13@gmail.com>
2025-07-10 12:35:24 -0600

    completed projec

--- main.js
+++ main.js
@@ -2,18 +2,31 @@
 function createGrid(userInput) {
     let gridSize = userInput ** 2;
     let boxSize = (960 / userInput);
+    
     for (let index = 0; index < gridSize; index++) {
         let newSquare = document.createElement('div');
         newSquare.setAttribute('class', 'box');
         container.appendChild(newSquare);
         newSquare.style.width = `${boxSize}px`;
         newSquare.style.height = `${boxSize}px`;
-        newSquare.addEventListener('mouseenter', (e) => newSquare.style.backgroundColor = "rgb("+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+")");
+        newSquare.addEventListener('mouseenter', () => newSquare.style.backgroundColor = "rgb("+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+")");
         }
-} 
+}
+
+function resetGrid() {
+    let userInput = prompt("Enter new canvas size: ");
+    while (userInput > 100) {
+       userInput = prompt("Enter new canvas size: "); 
+    } 
+    container.innerHTML = '';
+    createGrid(userInput);
+
+}
 
 let container = document.querySelector('#container');
-let userInput = 16 // attch to button 
+let resetButton = document.querySelector('button');
+let defaultSize = 16 // attch to button 
 
-createGrid(userInput);
+createGrid(defaultSize);
+resetButton.addEventListener('click', resetGrid);
 
