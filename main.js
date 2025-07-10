84aa4fb6d64501c2b52ff305b31c1e1a1d249c44
MountainChickadee <osmo13@gmail.com>
2025-07-10 11:57:27 -0600

    completed hover effect

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ function createGrid(userInput) {
         container.appendChild(newSquare);
         newSquare.style.width = `${boxSize}px`;
         newSquare.style.height = `${boxSize}px`;
-    }
+        newSquare.addEventListener('mouseenter', (e) => newSquare.style.backgroundColor = "rgb("+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+", "+Math.floor(Math.random() * 255)+")");
+        }
 } 
 
 let container = document.querySelector('#container');
 let userInput = 16 // attch to button 
 
 createGrid(userInput);
+
